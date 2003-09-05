88a675b9e660050c40a4a7a48836611c636dbcc1
fguillaume <devnull@localhost>
2003-09-05 17:43:47 +0000

    Reindent.

--- skins/cps_schemas/getDocumentVocabularies.py
+++ skins/cps_schemas/getDocumentVocabularies.py
@@ -6,15 +6,21 @@ Please, follow the same pattern to add new ones.
 """
 
 vocabularies = {
-        'dummy_voc': {
-            'data': {'dict': {'foo': "Foo",
-                              'bar': "Bar",
-                              'baz123': "Baz123",
-                              },
-                     'list': ['foo', 'bar', 'baz123'],
-                     },
+    'dummy_voc': {
+        'data': {
+            'dict': {
+                'foo': "Foo",
+                'bar': "Bar",
+                'baz123': "Baz123",
+                },
+            'list': [
+                'foo',
+                'bar',
+                'baz123',
+                ],
             },
-        }
+        },
+    }
 
 cvocabularies = context.getCustomDocumentVocabularies()
 
