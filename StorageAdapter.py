c4eabb41581d9358cb2d21517e27de2610c53f72
fguillaume <devnull@localhost>
2003-09-12 18:27:54 +0000

    Simplify code.

--- StorageAdapter.py
+++ StorageAdapter.py
@@ -50,37 +50,16 @@ class BasicStorageAdapter:
         self._schema = schema
 
     #
-    # Internal methods to be implemented for simple cases.
-    #
-
-    def _get(self, field_id):
-        """Get the value for one field."""
-        raise NotImplementedError
-
-    def _set(self, field_id, value):
-        """Set the value into one field."""
-        raise NotImplementedError
-
-    def _delete(self, field_id):
-        """Delete a field."""
-        # XXX this is useful for flex docs when the schema changes.
-        raise NotImplementedError
-
-    #
     # API called by DataModel
     #
 
     def getData(self):
         """Get data from the object, returns a mapping."""
-        data = {}
-        for field_id in self._schema.keys():
-            data[field_id] = self._get(field_id)
-        return data
+        raise NotImplementedError
 
     def setData(self, data):
         """Set data to the object, from a mapping."""
-        for field_id in self._schema.keys():
-            self._set(field_id, data[field_id])
+        raise NotImplementedError
 
 
 class AttributeStorageAdapter(BasicStorageAdapter):
@@ -106,10 +85,6 @@ class AttributeStorageAdapter(BasicStorageAdapter):
         This is used by CPS to switch to a writable object after unfreezing.
         """
         self._ob = ob
-
-    def _set(self, field_id, value):
-        """Set the value."""
-        setattr(self._ob, field_id, value)
 
     def _delete(self, field_id):
         raise NotImplementedError
@@ -137,3 +112,8 @@ class AttributeStorageAdapter(BasicStorageAdapter):
                 value = field.getDefault()
             data[field_id] = value
         return data
+
+    def setData(self, data):
+        """Set data to the object, from a mapping."""
+        for field_id in self._schema.keys():
+            setattr(self._ob, field_id, data[field_id])
