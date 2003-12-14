f74bf0d41778af1c584f56cddf3cffb5809a10bf
sfermigier <devnull@localhost>
2003-12-14 16:17:35 +0000

    Add + refactor basic widget tests.

--- tests/testWidgets.py
+++ tests/testWidgets.py
@@ -8,6 +8,22 @@ from Products.CPSSchemas.WidgetTypesTool import WidgetTypeRegistry
 
 class TestWidgetTypesTool(CPSSchemasTestCase.CPSSchemasTestCase):
 
+    def afterSetUp(self):
+        self.tool = self.portal.portal_widget_types
+
+        # Delete widget types that has already been set up by the CPSDocument
+        # installer.
+        self.tool.manage_delObjects(ids=list(self.tool.objectIds()))
+
+        # Refill the tool with the widgets using only default values.
+        amt = self.tool.all_meta_types()
+        for d in amt:
+            widget_type = d['name']
+            swt = widget_type.replace(" ", "")
+            id = widget_type[len("CPS "):-len(" Type")]
+            self.tool.manage_addCPSWidgetType(id, swt)
+
+
     def testRegistry(self):
         registry = WidgetTypeRegistry
 
@@ -27,19 +43,30 @@ class TestWidgetTypesTool(CPSSchemasTestCase.CPSSchemasTestCase):
             widget = widget_type_instance.makeInstance("widget_id")
             self.assert_(isinstance(widget, widget_class))
 
-    def testTool(self):
-        tool = self.portal.portal_widget_types
-        amt = tool.all_meta_types()
+            # XXX: doesn't work if the widget type doesn't live in
+            # portal_widget_types
+            # field_types = widget.getFieldTypes()
 
-    def _makeWidget(self, widget_id, widget_type):
-        widget_type_instance = WidgetTypeRegistry.makeWidgetTypeInstance(
-            widget_type, "widget_type_id")
-        widget = widget_type_instance.makeInstance(widget_id)
-        return widget
 
     def testStringWidget(self):
-        widget = self._makeWidget("widget_id", "CPS String Widget Type")
+        widget = self.tool["String Widget"].makeInstance("widget_id")
         self.assertEquals(widget.getWidgetId(), "widget_id")
+        self.assertEquals(widget.getFieldTypes(), ('CPS String Field',))
+
+    def testIntWidget(self):
+        widget = self.tool["Int Widget"].makeInstance("widget_id")
+        self.assertEquals(widget.getWidgetId(), "widget_id")
+        self.assertEquals(widget.getFieldTypes(), ('CPS Int Field',))
+
+    def testLongWidget(self):
+        widget = self.tool["Long Widget"].makeInstance("widget_id")
+        self.assertEquals(widget.getWidgetId(), "widget_id")
+        self.assertEquals(widget.getFieldTypes(), ('CPS Long Field',))
+
+    def testFloatWidget(self):
+        widget = self.tool["Float Widget"].makeInstance("widget_id")
+        self.assertEquals(widget.getWidgetId(), "widget_id")
+        self.assertEquals(widget.getFieldTypes(), ('CPS Float Field',))
 
 
 def test_suite():
