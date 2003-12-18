95afc897b2b4bf8fb1b97ec33039856af96e1ab3
hcauwelier <devnull@localhost>
2003-12-18 16:08:49 +0000

    step 1 of 2 for translating vocabulary interface

--- skins/cps_schemas/vocab_addkey.py
+++ skins/cps_schemas/vocab_addkey.py
@@ -1,4 +1,4 @@
-##parameters=voc_id, new_key, new_label, new_msgid, REQUEST
+#parameters=voc_id, new_key, new_label, new_msgid, REQUEST
 
 # Parameters:
 #
@@ -9,7 +9,7 @@
 vocabulary = context.portal_vocabularies[voc_id]
 vocabulary.manage_addVocabularyItem(new_key, new_label, new_msgid)
 
-psm = 'cpsschemas_psm_vocabulary_key_added'
+psm = 'cpsschemas_psm_vocabulary_entry_added'
 action_path = 'vocab_edit_form'
 REQUEST.RESPONSE.redirect('%s/%s?voc_id=%s&portal_status_message=%s' %
                           (context.absolute_url(), action_path, voc_id, psm))
