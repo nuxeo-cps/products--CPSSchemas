c64363a9a487a575421cf303440b8648e4a8d2b1
fguillaume <devnull@localhost>
2004-01-02 14:26:18 +0000

    Field property is_indexed has been renamed to is_searchabletext.

--- tests/testSchemas.py
+++ tests/testSchemas.py
@@ -9,13 +9,13 @@ import CPSSchemasTestCase
 
 metadata_schema = {
     'Title': {'type': 'CPS String Field',
-              'data': {'is_indexed': 1,}},
+              'data': {'is_searchabletext': 1,}},
     'Description': {'type': 'CPS String Field',
-                    'data': {'is_indexed': 1}},
+                    'data': {'is_searchabletext': 1}},
     'Subject': {'type': 'CPS String List Field',
-                'data': {'is_indexed': 1}},
+                'data': {'is_searchabletext': 1}},
     'Contributors': {'type': 'CPS String List Field',
-                     'data': {'is_indexed': 1}},
+                     'data': {'is_searchabletext': 1}},
     'CreationDate': {'type': 'CPS DateTime Field',
                      'data': {'write_ignore_storage': 1,}},
     'ModificationDate': {'type': 'CPS DateTime Field',
@@ -26,9 +26,9 @@ metadata_schema = {
                'data': {'write_ignore_storage': 1,}},
     'Language': {'type': 'CPS String Field',
                  'data': {'write_ignore_storage': 1,}},
-    'Rights': {'type': 'CPS String Field', 'data': {'is_indexed': 1}},
+    'Rights': {'type': 'CPS String Field', 'data': {'is_searchabletext': 1}},
     'Creator': {'type': 'CPS String Field',
-                'data': {'is_indexed': 1,
+                'data': {'is_searchabletext': 1,
                          'write_ignore_storage': 1,}},
     }
 
