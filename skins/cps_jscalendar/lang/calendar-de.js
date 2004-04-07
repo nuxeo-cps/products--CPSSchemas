b9e5c54ffb1b3f02130494f2a5af7f1521dd69cd
sfermigier <devnull@localhost>
2004-04-07 13:57:24 +0000

    German translation (thanks to Georg Hennemann).

---
+++ skins/cps_jscalendar/lang/calendar-de.js
@@ -0,0 +1,45 @@
+// ** I18N
+Calendar._DN = new Array
+("Sonntag",
+ "Montag",
+ "Dienstag",
+ "Mittwoch",
+ "Donnerstag",
+ "Freitag",
+ "Samstag",
+ "Sonntag");
+Calendar._MN = new Array
+("Januar",
+ "Februar",
+ "März",
+ "April",
+ "Mai",
+ "Juni",
+ "Juli",
+ "August",
+ "September",
+ "Oktober",
+ "November",
+ "Dezember");
+
+// tooltips
+Calendar._TT = {};
+Calendar._TT["TOGGLE"] = "Wechseln ersten Tag der Woche ";
+Calendar._TT["PREV_YEAR"] = "Letztes Jahr (halten für Menü)";
+Calendar._TT["PREV_MONTH"] = "Letzter Monat (halten für Menü)";
+Calendar._TT["GO_TODAY"] = "Gehe nach Heute";
+Calendar._TT["NEXT_MONTH"] = "Next month (halten für Menü)";
+Calendar._TT["NEXT_YEAR"] = "Nächstes Jahr (halten für Menü)";
+Calendar._TT["SEL_DATE"] = "Wähle Datum";
+Calendar._TT["DRAG_TO_MOVE"] = "Ziehe Maus ";
+Calendar._TT["PART_TODAY"] = " (heute)";
+Calendar._TT["MON_FIRST"] = "Mondag zuerst";
+Calendar._TT["SUN_FIRST"] = "Sonntag zuerst";
+Calendar._TT["CLOSE"] = "Schliessen";
+Calendar._TT["TODAY"] = "Heute";
+
+// date formats
+Calendar._TT["DEF_DATE_FORMAT"] = "y-mm-dd";
+Calendar._TT["TT_DATE_FORMAT"] = "D, M d";
+
+Calendar._TT["WK"] = "wo";
