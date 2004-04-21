200a295e31d78494995cdbf6c509143ef2f37361
bdelbosc <devnull@localhost>
2004-04-21 16:05:35 +0000

    adding renderLayout method

--- LayoutsTool.py
+++ LayoutsTool.py
@@ -24,9 +24,14 @@ The Layouts Tool manages layouts.
 from Globals import InitializeClass
 from AccessControl import ClassSecurityInfo
 
+from Products.CMFCore.CMFCorePermissions import View
 from Products.CMFCore.utils import UniqueObject
+from Products.CMFCore.utils import getToolByName
 
 from Products.CPSSchemas.Layout import LayoutContainer
+from Products.CPSSchemas.DataStructure import DataStructure
+from Products.CPSSchemas.DataModel import DataModel
+from Products.CPSSchemas.StorageAdapter import AttributeStorageAdapter
 
 
 class LayoutsTool(UniqueObject, LayoutContainer):
@@ -43,6 +48,55 @@ class LayoutsTool(UniqueObject, LayoutContainer):
     def __init__(self):
         LayoutContainer.__init__(self, self.id)
 
+    security.declareProtected(View, 'renderLayout')
+    def renderLayout(self, layout_id, schema_id, context, mapping=None,
+                     layout_mode='edit', **kw):
+        """Render a layout/schema.
+
+        Return rendered, msg, ds
+        rendered is the html rendering
+
+        if mapping is not none then the layout is validate
+        and msg is either 'valid' or 'invalid',
+
+        you can add kw like style_prefix."""
+        msg = ''
+        stool = getToolByName(self, 'portal_schemas')
+        ltool = getToolByName(self, 'portal_layouts')
+        schema = stool._getOb(schema_id)
+        layout = ltool._getOb(layout_id)
+        adapters = [AttributeStorageAdapter(schema, None)]
+        dm = DataModel(None, adapters, proxy=context, context=context)
+        dm._fetch()
+        dm._check_acls = 0 # this is needed to shortcut directory acl
+        ds = DataStructure(datamodel=dm)
+        layout.prepareLayoutWidgets(datastructure=ds)
+
+        if mapping:
+            ds.updateFromMapping(mapping)
+        dm = ds.getDataModel()
+        layout_structure = layout.computeLayoutStructure(
+            layout_mode=layout_mode,
+            datamodel=dm)
+        layout = layout_structure['layout']
+        if mapping:
+            if layout.validateLayoutStructure(layout_structure,
+                                              ds, layout_mode=layout_mode):
+                msg = 'valid'
+            else:
+                msg = 'invalid'
+
+        layout.renderLayoutStructure(layout_structure, ds,
+                                     layout_mode=layout_mode)
+
+        rendered = layout.renderLayoutStyle(layout_structure,
+                                            ds, context,
+                                            first_layout=0,
+                                            last_layout=0,
+                                            is_flexible=0,
+                                            layout_mode=layout_mode,
+                                            **kw)
+        return rendered, msg, ds
+
+
 InitializeClass(LayoutsTool)
-
-
