8ec72a87d9606299bfbfe5e25a52cdae19f8c387
sfermigier <devnull@localhost>
2004-07-19 13:04:23 +0000

    New german translation thanks to Georg Hennemann.

--- skins/cps_jscalendar/lang/calendar-de.js
+++ skins/cps_jscalendar/lang/calendar-de.js
@@ -1,45 +1,45 @@
 // ** I18N
 Calendar._DN = new Array
 ("Sonntag",
- "Montag",
- "Dienstag",
- "Mittwoch",
- "Donnerstag",
- "Freitag",
- "Samstag",
- "Sonntag");
+"Montag",
+"Dienstag",
+"Mittwoch",
+"Donnerstag",
+"Freitag",
+"Samstag",
+"Sonntag");
 Calendar._MN = new Array
 ("Januar",
- "Februar",
- "März",
- "April",
- "Mai",
- "Juni",
- "Juli",
- "August",
- "September",
- "Oktober",
- "November",
- "Dezember");
+"Februar",
+"März",
+"April",
+"Mai",
+"Juni",
+"Juli",
+"August",
+"September",
+"Oktober",
+"November",
+"Dezember");
 
 // tooltips
 Calendar._TT = {};
-Calendar._TT["TOGGLE"] = "Wechseln ersten Tag der Woche ";
-Calendar._TT["PREV_YEAR"] = "Letztes Jahr (halten für Menü)";
-Calendar._TT["PREV_MONTH"] = "Letzter Monat (halten für Menü)";
-Calendar._TT["GO_TODAY"] = "Gehe nach Heute";
-Calendar._TT["NEXT_MONTH"] = "Next month (halten für Menü)";
-Calendar._TT["NEXT_YEAR"] = "Nächstes Jahr (halten für Menü)";
-Calendar._TT["SEL_DATE"] = "Wähle Datum";
-Calendar._TT["DRAG_TO_MOVE"] = "Ziehe Maus ";
+Calendar._TT["TOGGLE"] = "Wechsel des Wochenbeginns";
+Calendar._TT["PREV_YEAR"] = "vorh. Jahr (halten f. Menü)";
+Calendar._TT["PREV_MONTH"] = "vorh. Monat (halten f. Menü)";
+Calendar._TT["GO_TODAY"] = "Heute";
+Calendar._TT["NEXT_MONTH"] = "nächst. Monat (halten f. Menü)";
+Calendar._TT["NEXT_YEAR"] = "nächst. Jahr (halten f. Menü)";
+Calendar._TT["SEL_DATE"] = "Datum wählen";
+Calendar._TT["DRAG_TO_MOVE"] = "Verschieben";
 Calendar._TT["PART_TODAY"] = " (heute)";
-Calendar._TT["MON_FIRST"] = "Mondag zuerst";
-Calendar._TT["SUN_FIRST"] = "Sonntag zuerst";
-Calendar._TT["CLOSE"] = "Schliessen";
+Calendar._TT["MON_FIRST"] = "Wochenanf. Montag";
+Calendar._TT["SUN_FIRST"] = "Wochenanf. Sonntag";
+Calendar._TT["CLOSE"] = "Schließen";
 Calendar._TT["TODAY"] = "Heute";
 
 // date formats
 Calendar._TT["DEF_DATE_FORMAT"] = "y-mm-dd";
 Calendar._TT["TT_DATE_FORMAT"] = "D, M d";
 
-Calendar._TT["WK"] = "wo";
+Calendar._TT["WK"] = "Wo";
