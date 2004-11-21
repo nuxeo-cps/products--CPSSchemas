b97c48ff98f6879d25937bc21c940e394eb430eb
jmorliaguet <devnull@localhost>
2004-11-21 21:14:19 +0000

    fix: added a security check when returning the list of links to be displayed to avoid raising an unauthorized exception on link items refering to protected objects.

--- skins/cps_schemas/getObjectsFromPath.py
+++ skins/cps_schemas/getObjectsFromPath.py
@@ -3,12 +3,16 @@ portal_url = context.portal_url
 portal = portal_url.getPortalObject()
 
 returned_list = []
+mtool = context.portal_membership
+checkPerm = mtool.checkPermission
 
 for path in path_list:
     try:
         if path.startswith('/'):
             path = path[1:]
         object = portal.restrictedTraverse(path)
+        if not checkPerm('View', object):
+            continue
         returned_list.append(object)
     except:
         pass
