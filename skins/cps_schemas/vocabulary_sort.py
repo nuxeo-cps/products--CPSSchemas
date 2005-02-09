b375449439a5e48bd2ef156094a7a7955785f5d0
edegoute <devnull@localhost>
2005-02-09 03:58:28 +0000

    duplicate code, but cleaner

--- skins/cps_schemas/vocabulary_sort.py
+++ skins/cps_schemas/vocabulary_sort.py
@@ -10,11 +10,12 @@ voc = context.portal_vocabularies[voc_id]
 
 voc_items = voc.items()
 if msgid == 0:
-    items = [ (x[1], x[0]) for x in voc_items]
-elif msgid == 1:
-    items = [ (cpsmcat(voc.getMsgid(x[0])), x[0]) for x in voc_items]
-items.sort()
-
-return [x[1] for x in items]
+    l = [ (x[1], x[0]) for x in voc_items]
+    l.sort()
+    return [x[1] for x in l]
+else:
+    l = [ (cpsmcat(voc.getMsgid(x[0])), x[0]) for x in voc_items]
+    l.sort()
+    return [x[1] for x in l]
 
 
