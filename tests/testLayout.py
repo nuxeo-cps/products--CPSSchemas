4d1ea79fa96b91f1807e15fc7160cb647fc1a6e2
fguillaume <devnull@localhost>
2005-02-24 17:06:57 +0000

    Cleanup.

--- tests/testLayout.py
+++ tests/testLayout.py
@@ -1,16 +1,32 @@
-# (c) 2003 Nuxeo SARL <http://nuxeo.com>
+# Copyright (c) 2003-2005 Nuxeo SARL <http://nuxeo.com>
+# Authors: Tarek Ziade <tz@nuxeo.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
 # $Id$
 
-import unittest, os
+import unittest
+import os
+from Acquisition import Implicit
 from Products.CPSSchemas.Layout import Layout, CPSLayout
 from Products.CPSSchemas.Schema import CPSSchema
 from Products.CPSSchemas.DataStructure import DataStructure
 from Products.CPSSchemas.DataModel import DataModel
 from Products.CPSSchemas.StorageAdapter import AttributeStorageAdapter
-from Acquisition import Implicit
-from Testing.ZopeTestCase import ZopeTestCase
-from Products.CPSSchemas.BasicWidgets import CPSStringWidgetType, \
-     CPSIntWidgetType
+from Products.CPSSchemas.BasicWidgets import CPSStringWidgetType
+from Products.CPSSchemas.BasicWidgets import CPSIntWidgetType
 
 from Products.CPSSchemas.tests import __file__ as landmark
 
@@ -44,23 +60,21 @@ def openfile(filename, mode='r'):
     path = os.path.join(os.path.dirname(landmark), 'scripts', filename)
     return open(path, mode)
 
-class LayoutTests(ZopeTestCase):
+
+class LayoutTests(unittest.TestCase):
 
     def test_InstanciateLayout(self):
-        """ simple Layout instanciation
-        """
+        # simple Layout instanciation
         ob = Layout()
         self.assertNotEquals(ob, None)
 
     def test_InstanciateCPSLayout(self):
-        """ simple CPSLayout instanciation
-        """
+        # simple CPSLayout instanciation
         ob = CPSLayout('my_layout')
         self.assertNotEquals(ob, None)
 
     def test_addingWidgets(self):
-        """ adding a few widgets
-        """
+        # adding a few widgets
         ob = CPSLayout('my_layout').__of__(fakePortal)
         ob.addWidget('my_int', 'Int Widget')
         ob.addWidget('my_int2', 'Int Widget')
@@ -68,8 +82,7 @@ class LayoutTests(ZopeTestCase):
         return ob
 
     def test_setLayoutDefinition(self):
-        """ Validate setLayoutDefinition
-        """
+        # Validate setLayoutDefinition
         layout = self.test_addingWidgets()
 
         # setting the layout definition
@@ -82,14 +95,13 @@ class LayoutTests(ZopeTestCase):
         layout.setLayoutDefinition(layoutdef)
         layoutdef = layout.getLayoutDefinition()
         layoutdef = layout.getLayoutDefinition()
-        self.assertEquals(layoutdef['rows'],  [[{'widget_id': 'my_int', 'ncols': 1},],
+        self.assertEquals(layoutdef['rows'],  [[{'widget_id': 'my_int',
+                                                 'ncols': 1},],
                 [{'widget_id': 'my_int2', 'ncols': 1},],
                [{'widget_id': 'my_string', 'ncols': 1},]])
         return layout
 
     def test_prepareLayoutWidgets(self):
-        """ test prepareLayoutWidgets
-        """
         doc = FakeDocument()
         layout = self.test_setLayoutDefinition()
 
@@ -113,8 +125,6 @@ class LayoutTests(ZopeTestCase):
         return layout, dm, datastructure
 
     def test_computeLayoutStructure(self):
-        """ test computeLayoutStructure
-        """
         layout_plus = self.test_prepareLayoutWidgets()
         layout = layout_plus[0]
         dm = layout_plus[1]
@@ -126,8 +136,6 @@ class LayoutTests(ZopeTestCase):
         return layout, ls, dm, ds
 
     def test_validateLayoutStructure(self):
-        """ test validateLayoutStructure
-        """
         layout_plus = self.test_computeLayoutStructure()
         layout = layout_plus[0]
         ds = layout_plus[3]
@@ -138,8 +146,6 @@ class LayoutTests(ZopeTestCase):
 
 
     def test_validateLayoutStructureWithScripting(self):
-        """ test validateLayoutStructure with scripting
-        """
         layout_plus = self.test_computeLayoutStructure()
         layout = layout_plus[0]
         ds = layout_plus[3]
@@ -157,16 +163,20 @@ class LayoutTests(ZopeTestCase):
         my_script_content = my_script.readlines()
         my_script_content = ''.join(my_script_content)
 
-        layout.validate_values_expr = "python:datastructure['my_int'] > datastructure['my_int2']"
+        layout.validate_values_expr = (
+            "python:datastructure['my_int'] > datastructure['my_int2']")
         validation = layout.validateLayoutStructure(ls, ds)
         self.assertEquals(validation, False)
 
-        layout.validate_values_expr = "python:datastructure['my_int'] < datastructure['my_int2']"
+        layout.validate_values_expr = (
+            "python:datastructure['my_int'] < datastructure['my_int2']")
         validation = layout.validateLayoutStructure(ls, ds)
         self.assertEquals(validation, True)
 
 def test_suite():
-    return unittest.makeSuite(LayoutTests)
+    return unittest.TestSuite((
+        unittest.makeSuite(LayoutTests),
+        ))
 
-if __name__=="__main__":
-    unittest.main(defaultTest)
+if __name__ == '__main__':
+    unittest.TextTestRunner().run(test_suite())
