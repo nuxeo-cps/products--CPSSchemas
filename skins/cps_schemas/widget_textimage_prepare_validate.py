cf4828592c07b748efb59c512d22616faf5aa76a
madarche <devnull@localhost>
2005-03-03 15:49:23 +0000

    - Made the Text Image Widget be able to handle either 1 block or 2 blocks of   text.   This feature is now used by the News and PressRelease document types.

--- skins/cps_schemas/widget_textimage_prepare_validate.py
+++ skins/cps_schemas/widget_textimage_prepare_validate.py
@@ -2,22 +2,27 @@
 # $Id$
 
 if mode != 'validate':
-    # we handle only validation
+    # We handle only validation
     return
 
 if post_validate:
-    # we do nothing on post validation
+    # We do nothing on post validation
     return 1
 
-# changing position of text if there is 2 column
+# Changing position of text if there is 2 column
 content_wid = context.widget_ids[1]
-content_right_wid = context.widget_ids[2]
-if len(datastructure[content_right_wid]):
-    # column mode
+if len(context.widget_ids) > 2:
+    # Schema can handle either 1 block or 2 blocks of text
+    content_right_wid = context.widget_ids[2]
+else:
+    content_right_wid = ''
+
+if content_right_wid and len(datastructure[content_right_wid]):
+    # Column mode
     datastructure.set(content_wid + '_rposition', 'col_left')
     datastructure.set(content_right_wid + '_rposition', 'col_right')
 else:
-    # only one block of text
+    # Only one block of text
     datastructure.set(content_wid + '_rposition', 'normal')
     datastructure.set(content_right_wid + '_rposition', 'normal')
 
