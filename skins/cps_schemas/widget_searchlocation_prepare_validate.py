550ee2513d718b90c156873f60c4e9d82ff7a48c
atchertchian <devnull@localhost>
2005-11-10 18:22:24 +0000

    use recommended methods to get current rpath for search

--- skins/cps_schemas/widget_searchlocation_prepare_validate.py
+++ skins/cps_schemas/widget_searchlocation_prepare_validate.py
@@ -2,18 +2,23 @@
 # $Id$
 """Prepare and Validate SearchLocation widget.
 
-Init value with current path.
+Init value with current relative path. Relative path does not take care of
+virtual hosting since catalog does not take care of it.
 """
+from Products.CMFCore.utils import getToolByName
+
+utool = getToolByName(context, 'portal_url')
 
 datamodel = datastructure.getDataModel()
 widget_id = context.getWidgetId()
 field_id = context.fields[0]
-purl = context.portal_url
-portal_path = '/'.join(purl.getPhysicalPath()[:-1])
+
+portal_path = utool.getPortalPath()
 
 if mode == 'prepare':
     proxy = datamodel.getProxy()
-    rpath = proxy.absolute_url(relative=1)[len(portal_path):]
+    ppath = '/'.join(proxy.getPhysicalPath())
+    rpath = ppath[len(portal_path)+1:]
     datastructure.set(widget_id, rpath)
     datastructure.set(widget_id+'_select', 'site')
     return
