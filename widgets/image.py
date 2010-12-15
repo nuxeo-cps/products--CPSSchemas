57244fc5594b0716a7c6b5aafebab46b6ebc245a
Georges Racinet on purity.racinet.fr <georges@racinet.fr>
2010-12-15 12:44:24 +0100

    First working iteration on Photo widget (relies on CPSUtil.image)

--- widgets/image.py
+++ widgets/image.py
@@ -16,6 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
+import urllib
 
 from Globals import InitializeClass
 from OFS.Image import Image
@@ -23,12 +24,18 @@ from zExceptions import BadRequest
 
 from Products.CPSUtil.html import renderHtmlTag
 from Products.CPSUtil.mail import make_cid
+from Products.CPSUtil.image import resized_img_geometry
+from Products.CPSUtil.image import parse_size_spec_as_dict
 from Products.CPSSchemas.Widget import EMAIL_LAYOUT_MODE
 from Products.CPSSchemas.BasicWidgets import CPSFileWidget, CPSIntWidget
 from Products.CPSSchemas.BasicWidgets import CPSProgrammerCompoundWidget
 from Products.CPSSchemas.Widget import CIDPARTS_KEY
 
 logger = logging.getLogger(__name__)
+
+class MissingSizeWidget(IndexError):
+    """Exception raised if a size widget does not exist (not always an error).
+    """
 
 class CPSImageWidget(CPSFileWidget, CPSProgrammerCompoundWidget):
     """Image widget.
@@ -64,13 +71,17 @@ class CPSImageWidget(CPSFileWidget, CPSProgrammerCompoundWidget):
         dict(id='size_spec', type='string', mode='w',
          label='Size specification (e.g. 320x200, w430, h200, l320, full)'),
         dict(id='widget_ids', type='tokens', mode='w',
-             label='Auxiliary widgets for size specifications '
-             '(e.g. 320x200, w430, h200, l320, full)'),
+             label='Auxiliary widgets for user-managed size specifications '),
         )
 
     size_spec = ''
     widget_ids = ()
     allow_resize = True
+
+    size_widgets = (0,) # sub widgets that handle size specs, if defined
+
+    render_method = 'renderAsCompound' # called by CPSCompoundWidget.render
+    final_render_method = 'widget_image_render' # called by renderAsCompound
 
     def updateImageInfoForEmail(self, info, datastructure, dump=False):
         """Use the cid: URL scheme (RFC 2392) and dump parts in datastructure.
@@ -141,66 +152,86 @@ class CPSImageWidget(CPSFileWidget, CPSProgrammerCompoundWidget):
         alt = ds.get(wid + '_alt', title)
         info['alt'] = alt
 
+        # image URI
         if layout_mode == EMAIL_LAYOUT_MODE:
             uri = info['content_url']
-        else: # generate resizing URI
-            # TODO -> CPSUtil
-            from Products.CPSCore.ProxyBase import ImageDownloader
-            size_spec = self.getSizeSpec(ds)
-            parsed_spec = ImageDownloader._parseSizeSpecAsDict(size_spec)
-            dm = ds.getDataModel()
-            uri = dm.imageUri(self.fields[0], **parsed_spec)
+        else:
+            uri = self.imageUri(ds.getDataModel())
         info['image_tag'] = renderHtmlTag('img', src=uri, title=title, alt=alt)
 
         return info
 
-    def getSizeSpec(self, ds):
-        """Return a usable size spec from what's in ds' datamodel.
+    def imageUri(self, dm, w_index=0, if_w_missing='prop'):
+        """Return a sized URI for the image.
+        w_index can be used to select the size widget responsible for the spec.
+        if_w_missing controls what to do if the size widget is missing.
+        values : 'prop' (default to widget prop)
+                 'full' (default to full size)
+        """
+        try:
+            spec = self.getSizeSpec(dm, w_index=w_index)
+        except MissingSizeWidget:
+            if if_w_missing == 'prop':
+                spec = self.size_spec
+            elif if_w_missing == 'full':
+                spec = 'full'
+            else:
+                raise
+
+        parsed_spec = parse_size_spec_as_dict(spec)
+        return dm.imageUri(self.fields[0], **parsed_spec)
+
+    def getSizeSpec(self, dm, w_index=0, with_widget=False):
+        """Return a usable size spec from what's in datamodel.
 
         If the dm value evaluates to False, the widget property is used.
+        The optional w_index allows to choose among subwidgets
+        if with_widget is True, the widget is also returned, or None
         """
-        if not self.widget_ids:
-            return self.size_spec
+        try:
+            swidget = self._getSubWidgets()[w_index]
+        except IndexError:
+            raise MissingSizeWidget(w_index)
 
-        # GR: this in almost always equivalent to self.widget_ids[0]
-        # thank you ZODB cache
-        swidget = self.getSizeWidget()
-        v = ds.getDataModel()[swidget.fields[0]]
+        v = dm[swidget.fields[0]]
         if not v:
-            return self.size_spec
+            res = self.size_spec
         elif isinstance(v, int):
-            return 'l%d' % v
-        return v
+            res = 'l%d' % v
+        else:
+            res = v
+        return with_widget and (res, swidget) or res
 
-    def prepare(self, datastructure, **kw):
-        """Prepare datastructure from datamodel."""
-
-        CPSFileWidget.prepare(self, datastructure, **kw)
-        datamodel = datastructure.getDataModel()
-        widget_id = self.getWidgetId()
-
+    def mainPrepare(self, ds, **kw):
+        """Part of preparation that does not depend on optional fields."""
+        CPSFileWidget.prepare(self, ds, **kw)
         if self.widget_ids:
             for widget in self._getSubWidgets():
-                widget.prepare(datastructure, **kw)
+                widget.prepare(ds, **kw)
+
+    def optPrepare(self, ds, **kw):
+        """Prepare datastructure from datamodel using optional fields"""
+        dm = ds.getDataModel()
+        widget_id = self.getWidgetId()
 
         title = ''
         if len(self.fields) > 2:
-            title = datamodel[self.fields[2]]
-        datastructure[widget_id + '_title'] = title
+            title = dm[self.fields[2]]
+        ds[widget_id + '_title'] = title
 
         alt = ''
         if len(self.fields) > 2:
-            alt = datamodel[self.fields[2]]
+            alt = dm[self.fields[2]]
             # Defaulting to the file name if there is an image file and if no
             # alt has been given yet. This is the case when the document is
             # created.
             if not alt:
-                alt = datastructure[widget_id + '_filename']
-        datastructure[widget_id + '_alt'] = alt
+                alt = ds[widget_id + '_filename']
+        ds[widget_id + '_alt'] = alt
 
-    def getSizeWidget(self):
-        if self.widget_ids:
-            return self._getSubWidgets()[0]
+    def prepare(self, ds, **kw):
+        self.mainPrepare(ds, **kw)
+        self.optPrepare(ds, **kw)
 
     def validate(self, ds, **kw):
         """Override FileWidget's method."""
@@ -223,14 +254,26 @@ class CPSImageWidget(CPSFileWidget, CPSProgrammerCompoundWidget):
         if len(self.fields) > 3:
             dm[self.fields[3]] = ds[widget_id + '_alt']
 
-        if not self.widget_ids:
+        for i in self.size_widgets:
+            if not self.validateSizeSpec(dm, i):
+                return False
+
+        return True
+
+    def validateSizeSpec(self, dm, widget_index):
+        """Validate the size specification handled by sub widget #widget_index.
+        """
+        try:
+            spec, swidget = self.getSizeSpec(dm, w_index=widget_index,
+                                             with_widget=True)
+        except MissingSizeWidget:
             return True
 
-        spec = self.getSizeSpec(ds)
-        from Products.CPSCore.ProxyBase import ImageDownloader
         try:
-            ImageDownloader._parseSizeSpec(spec)
-        except BadRequest:
+            parse_size_spec(spec)
+        except ValueError:
+            if swidget is None:
+                raise
             ds.setError(swidget.getWidgetId(),
                         'cpsschemas_invalid_size_spec')
             return False
@@ -245,8 +288,6 @@ class CPSImageWidget(CPSFileWidget, CPSProgrammerCompoundWidget):
             return '', {}
         return 'cpsschemas_err_image', {}
 
-    render_method = 'fullEditRender'
-
     def render(self, mode, datastructure, **kw):
         img_info = self.getImageInfo(datastructure, dump_cid_parts=True, **kw)
         if mode == 'view':
@@ -257,26 +298,152 @@ class CPSImageWidget(CPSFileWidget, CPSProgrammerCompoundWidget):
         return CPSProgrammerCompoundWidget.render(self, mode, datastructure,
                                                   img_info=img_info, **kw)
 
-
-    def fullEditRender(self, mode='edit', datastructure=None,
-                       cells=(), img_info=None, **kw):
-        """Final rendering method for the compound case."""
+    def renderAsCompound(self, mode='edit', datastructure=None, cells=(),
+                         img_info=None, **kw):
+        """Final rendering method for the compound case.
+        """
 
         if datastructure is None or img_info is None:
             # should really not happen
             raise ValueError('datastructure or img_info is None')
 
-        render_method = 'widget_image_render'
+        render_method = self.final_render_method
         if cells:
             size_widget = cells[0]
         else:
             size_widget = None
+
+        kw.update(img_info)
         meth = getattr(self, render_method, None)
         if meth is None:
             raise RuntimeError("Unknown Render Method %s for widget type %s"
                                % (render_method, self.getId()))
-        return meth(mode=mode, size_widget=cells and cells[0] or None,
-                    datastructure=datastructure, **img_info)
+        return meth(size_widget=cells and cells[0] or None, mode=mode,
+                    datastructure=datastructure, **kw)
 
 InitializeClass(CPSImageWidget)
+
+
+class CPSPhotoWidget(CPSImageWidget):
+    """Photo widget.
+
+    has more content-dependent options than plain Image Widget:
+      - caption (aka subtitle)
+      - user-configurable position (or not)
+      - popup for separate image viewing (typically bigger)
+
+    Overall, has potentially more rendering options than Image Widget.
+    New rendering options should be added to this widget rather than to Image
+    Widget
+    """
+    meta_type = 'Photo Widget'
+
+    field_types = ('CPS Image Field',   # Original image
+                   'CPS String Field',  # Caption
+                   'CPS String Field',  # render_position if configurable
+                   'CPS String Field',  # Title
+                   'CPS String Field',  # Alternate text for accessibility
+                   )
+
+    field_inits = ({}, {'is_searchabletext': 1,}, {}, {}, {})
+
+    _properties = CPSImageWidget._properties + (
+        {'id': 'render_position', 'type': 'selection', 'mode': 'w',
+         'select_variable': 'all_render_positions',
+         'label': 'Render position'},
+        {'id': 'configurable', 'type': 'selection', 'mode': 'w',
+         'select_variable': 'all_configurable',
+         'label': 'What is user configurable, require extra fields'},
+        {'id': 'zoom_size_spec', 'type': 'string', 'mode': 'w',
+         'label': 'Size specification for the zoomed display'},
+        )
+    all_configurable = ['nothing', 'position']
+    all_render_positions = ['left', 'center', 'right']
+
+    render_position = all_render_positions[0]
+    configurable = all_configurable[0]
+    zoom_size_spec = 'full'
+
+    final_render_method = 'widget_photo_render' # called by fullEditRender
+
+    def optPrepare(self, datastructure, **kw):
+        """Prepare datastructure from datamodel using optional fields"""
+        datamodel = datastructure.getDataModel()
+        widget_id = self.getWidgetId()
+
+        if len(self.fields) > 1:
+            datastructure[widget_id + '_subtitle'] = datamodel[self.fields[1]]
+        else:
+            datastructure[widget_id + '_subtitle'] = ''
+
+        rposition = self.render_position
+        if self.configurable != 'nothing':
+            if len(self.fields) > 2:
+                v = datamodel[self.fields[2]]
+                if v in self.all_render_positions:
+                    rposition = v
+        datastructure[widget_id + '_rposition'] = rposition
+
+        title = ''
+        if len(self.fields) > 4:
+            title = datamodel[self.fields[4]]
+        datastructure[widget_id + '_title'] = title
+
+        alt = ''
+        if len(self.fields) > 5:
+            alt = datamodel[self.fields[5]] # Same as in Image Widget
+            if not alt:
+                alt = datastructure[widget_id + '_filename']
+        datastructure[widget_id + '_alt'] = alt
+
+    def otherProcessing(self, choice, datastructure):
+        datamodel = datastructure.getDataModel()
+        widget_id = self.getWidgetId()
+
+        # Caption
+        if len(self.fields) > 1:
+            subtitle = datastructure[widget_id + '_subtitle']
+            datamodel[self.fields[1]] = subtitle
+
+        # Position
+        if self.configurable != 'nothing' and len(self.fields) > 2:
+            rposition = datastructure[widget_id + '_rposition']
+            if rposition and rposition in self.all_render_positions:
+                datamodel[self.fields[2]] = rposition
+
+    def getZoomInfo(self, ds):
+        """Extract info about zoomed image from datastructure."""
+        dm = ds.getDataModel()
+        try:
+            spec = self.getSizeSpec(dm, w_index=1)
+        except MissingSizeWidget:
+            spec = self.zoom_size_spec
+        wid = self.getWidgetId()
+        uri = dm.imageUri(self.fields[0], **parse_size_spec_as_dict(spec))
+        w, h = resized_img_geometry(ds[wid], spec)
+        return dict(uri=uri, width=w, height=h,
+                    escaped_uri=urllib.quote_plus(uri))
+
+    def render(self, mode, ds, **kw):
+        """Render in mode from datastructure."""
+        img_info = self.getImageInfo(ds, dump_cid_parts=True, **kw)
+        if mode == 'view':
+            if img_info['empty_file']:
+                return ''
+
+        widget_id = self.getWidgetId()
+        rposition = ds[widget_id + '_rposition']
+        subtitle = ds[widget_id + '_subtitle']
+
+        return CPSProgrammerCompoundWidget.render(
+            self, mode, ds,
+            subtitle=subtitle,
+            render_position=rposition,
+            configurable=str(self.configurable),
+            zoom=self.getZoomInfo(ds),
+            img_info=self.getImageInfo(ds, dump_cid_parts=True, **kw),
+            **kw)
+
+
+InitializeClass(CPSPhotoWidget)
 
